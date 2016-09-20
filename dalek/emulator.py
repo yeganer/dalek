5889dbcd0eb482aadba09f44945d7a2bfef75378
yeganer <yeganer@gmx.de>
2016-09-20 16:03:30 +0200

    Add an emulator with independent GPs

--- dalek/emulator.py
+++ dalek/emulator.py
@@ -60,3 +60,63 @@ class LogEmulator(Emulator):
     def comp_to_spectrum(self, Y):
         return 10 ** super(LogEmulator, self).comp_to_spectrum(Y)
 
+
+class DecoupledGaussianProcess(object):
+
+    def __init__(self, N, *args, **kwargs):
+        self.N = N
+        self._GP = [
+                sklearn.gaussian_process.GaussianProcess(*args, **kwargs)
+                for _ in xrange(N)
+                ]
+        self.y = None
+
+    def fit(self, X, y):
+        assert y.shape[1] == self.N
+        self.y = y
+        for i in xrange(self.N):
+            self._GP[i].fit(X, y[:, i])
+
+    def predict(self, X, eval_MSE=False):
+        X = np.atleast_2d(X)
+        y = np.zeros((X.shape[0], self.N))
+        if eval_MSE:
+            MSE = np.zeros((X.shape[0], self.N))
+            for i in xrange(self.N):
+                y[:, i], MSE[:, i] = self._GP[i].predict(X, eval_MSE=eval_MSE)
+            return y, MSE
+        else:
+            for i in xrange(self.N):
+                y[:, i] = self._GP[i].predict(X)
+            return y
+
+
+    # def __getattr__(self, name):
+    #     try:
+    #         getattr(self._GP[0], name).__call__
+    #     except AttributeError:
+    #         return getattr(self._GP[0], name)
+    #     else:
+    #         def f(*args, **kwargs):
+    #             for GP in self._GP:
+    #                 getattr(GP, name)(*args, **kwargs)
+    #         return f
+
+
+class DecoupledEmulator(Emulator):
+
+    def __init__(self, X, Y, n_components):
+        # Use GaussianProcessRegressor later
+        self._ncomp = n_components
+        self._PCA = sklearn.decomposition.PCA(n_components=n_components)
+        self._GP = DecoupledGaussianProcess(
+                n_components,
+                theta0=1e-2,
+                thetaL=1e-4,
+                thetaU=1e-1,
+                storage_mode='light',
+                )
+        self.train(X, Y)
+
+    def predict(self, *args, **kwargs):
+        raise NotImplementedError
